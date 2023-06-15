c51404658e7e6d7a1a4091cff8b1ffab63312225
Steven On <stevenon916@gmail.com>
2023-06-15 09:33:17 -0700

    Update GoToAssignment.cs

--- GoToAssignment.cs
+++ GoToAssignment.cs
@@ -23,44 +23,49 @@ namespace Client.Buttons
             const string TableDataWidth = "100%";
             const string TableDataStyle = "vertical-align:top;";
             string ButtonID = "btn" + item.Key;
-            string OnClick = "Client.Buttons.GoToAssignment.openAssigment(" + item.Key + ")";
+            string OnClick = "Client.Buttons.GoToAssignment.openAssigment(" + item.Key + ")"; // allows the use of OpenAssignment method
             const string ButtonClass = "button1 darkBlueButton highlightEnable";
             const string ButtonStyle = "width:100px;height:30px;border-radius:8px;";
             const string ButtonText = "Open";
             
             const string HTML = "<div><div class='{0}'><table style='{1}'><tbody><tr><td width='{2}' style='{3}'><div objname='ButtonPanel'><button id={4} onClick={5} objname='ButtonControl' class='{6}' style='{7}'>{8}</button></div></td></tr></tbody></table></div></div>";
             string htmlContent = String.Format(HTML, DivClass, TableStyle, TableDataWidth, TableDataStyle, ButtonID, OnClick, ButtonClass, ButtonStyle, ButtonText);
-            #endregion
             
             jQueryObject e = jQuery.FromHtml(htmlContent);
+            #endregion
             
             return e.GetElement(0);
         }
-        public void SetInlineWidgetSettings(string strInlineWidgetSettings)
-        {
-            return;
-        }
-
+        
         public static void OpenAssigment(int taskKey)
         {
+            // when this method is called, stop any further propagation for this event to allow for code below to execute
             Script.Literal("window.event.stopPropagation()");
+            
+            // retrieve the assignment key of the task
             ControllerServices.GetAssignmentByTaskKey(taskKey, delegate (W6.ClickMobile.Web.ObjectModel.W6CMAssignment assignment)
             {
                 try
                 {
+                    // open the assignment form when Open button is clicked
                     ControllerServices.OpenFormByKey(0, assignment.Key, false, successCallback);
                 }
                 catch (Exception ex)
                 {
                     ControllerServices.ErrorLog(ex);
                 }
-
             });
         }
 
+        // asynch methods below are created per ClickMobile API requirements but not used
         private static void successCallback(OpenFormStatus eOpenFormSuccess)
         {
             return;
         }
+        
+        public void SetInlineWidgetSettings(string strInlineWidgetSettings)
+        {
+            return;
+        }        
     }
 }
